f7c5422d82ef5fa010cc64c2d5fd5dc10ed1013e
Matt Pollock <mpollock@mitre.org>
2020-02-24 20:19:00 -0500

    mapproj suggests

--- tests/testthat/test_pkg_name.R
+++
@@ -1,6 +0,0 @@
-context("package name tests")
-
-test_that("package is named correctly", {
-  DESC_path <- ifelse(file.exists('DESCRIPTION'), 'DESCRIPTION', '../../DESCRIPTION')
-  expect_true(read.dcf(DESC_path)[,"Package"]=="aviationgeoms")
-})
